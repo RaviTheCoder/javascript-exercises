dbc182aab85c0012b99b4cfe8b95391683698da7
RaviTheCoder <rrravitesh@gmail.com>
2022-10-08 19:27:05 +1300

    Complete Ex 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,25 @@
-const repeatString = function() {
-
+const repeatString = function(text, num) {
+    let i = 0;
+    let stringString = '';
+    if (num < 0) {
+        return "ERROR";
+    }
+    while (i < num) {
+        stringString += text;
+        i++;
+    }
+    return stringString;
 };
+
+let stringString = '';
+
+function manyString(text, num) {
+    let i = 0;
+    while (i < num) {
+        stringString += text;
+    }
+    i++;
+}
 
 // Do not edit below this line
 module.exports = repeatString;
