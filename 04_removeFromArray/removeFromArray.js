71323914615e779983bf2c7b97dbee301f451609
Ravitesh Ratnam <rrravitesh@gmail.com>
2022-10-09 00:18:14 +1300

    Can remove multiple values

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,18 +1,24 @@
-const removeFromArray = function(array, value) {
-
-    for (let i = 0; i < array.length - 1; i++) {
-        if (array[i] == value) {
-            // console.log(i);
-            // console.log(array[i]);
-            // console.log(value);
-            array.splice(i, 1);
-            console.log(array);
-        } 
+const removeFromArray = function(array, ...value) {
+    console.log(value.length);
+    console.log(value[0]);
+    for (let i = 0; i < value.length; i++) {
+        for (let j = 0; j < array.length; j++) {
+            
+            if (array[j] == value[i]) {
+                console.log(array[j]);
+                console.log(value[i]);
+                // console.log(array[i]);
+                // console.log(value);
+                array.splice(j, 1);
+                console.log(array);
+            } 
+        }
     }
+    console.log(array);
     return array;
 };
 
-removeFromArray([1, 2, 3, 4], 3);
+removeFromArray([1, 2, 3, 4], 3, 2);
 
 // Do not edit below this line
 module.exports = removeFromArray;
