4926a10ff309b5b479fadaa4e9d72d4d7585dfc6
Ravitesh Ratnam <rrravitesh@gmail.com>
2022-10-08 23:50:43 +1300

    Can remove single value only

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,13 +1,18 @@
 const removeFromArray = function(array, value) {
-    let newArray = [];
-    for (let i = 0; i < array.length - 1; i++) {
-        if (array[i] != value) {
-            newArray = array.splice[i, 1];
-        } 
-        return array;
-    }
 
+    for (let i = 0; i < array.length - 1; i++) {
+        if (array[i] == value) {
+            // console.log(i);
+            // console.log(array[i]);
+            // console.log(value);
+            array.splice(i, 1);
+            console.log(array);
+        } 
+    }
+    return array;
 };
+
+removeFromArray([1, 2, 3, 4], 3);
 
 // Do not edit below this line
 module.exports = removeFromArray;
