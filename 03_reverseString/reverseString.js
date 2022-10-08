ad6a3df20f7a9b61a1c80da72bcb13d98379d977
RaviTheCoder <rrravitesh@gmail.com>
2022-10-08 19:41:22 +1300

    Complete Ex 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(text) {
+    let reversed = '';
+    i = 0;
+    while (i < text.length) {
+        reversed = text.charAt(i) + reversed;
+        i++;
+    }
+    return reversed;
 };
 
 // Do not edit below this line
