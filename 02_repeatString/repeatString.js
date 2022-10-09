6e528d026e180e3033e45cbe41a3306a687b5412
Ravitesh Ratnam <rrravitesh@gmail.com>
2022-10-09 17:59:55 +1300

    Complete Revision 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,21 @@
-const repeatString = function() {
+const repeatString = function(text, num) {
+    
+    if (num < 0) {
+        return 'ERROR';
+    }
+    
+    let i = 0;
+    let finalString = '';
+    
+    while (i < num) {
+        finalString += text;
+        i++;
+    }
 
+    return finalString;
 };
 
 // Do not edit below this line
 module.exports = repeatString;
+
+repeatString('hey', 3);
