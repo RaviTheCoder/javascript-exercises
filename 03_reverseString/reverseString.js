c8b22abdfe31a337afff82a7d2b570f2231c68f6
Ravitesh Ratnam <rrravitesh@gmail.com>
2022-10-09 18:08:38 +1300

    Revise Ex 04

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,16 @@
-const reverseString = function() {
+const reverseString = function(text) {
+    let i = 0;
+    let finalString = '';
+    console.log(text.length);
 
+    while (i < text.length) {
+        finalString += text.charAt(text.length - (i + 1));
+        i++;
+    }
+    return finalString;
 };
+
+reverseString('hello');
 
 // Do not edit below this line
 module.exports = reverseString;
