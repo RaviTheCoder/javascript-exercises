3a40746e7a8afd1d3ebab8d926713604ef97a467
Ravi Ratnam <raviratnam@outlook.com>
2022-10-10 00:06:45 +1300

    Complete Ex 06

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,26 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    if (typeof num1 != "number" || typeof num2 != "number") {
+        return 'ERROR';
+    }
+    
+    let lowNum = Math.min(num1, num2);
+    let highNum = Math.max(num1, num2);
+    let finalSum = 0;
 
+    if (lowNum < 0) {
+        return 'ERROR';
+    }
+
+    while (lowNum <= highNum) {
+        finalSum += lowNum;
+        lowNum++;
+    }
+
+    return finalSum;
 };
+
+sumAll(1, 4);
+
 
 // Do not edit below this line
 module.exports = sumAll;
