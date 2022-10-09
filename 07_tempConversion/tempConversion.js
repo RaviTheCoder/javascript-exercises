f2dc4f083c5778bd4b9844a05328b63dcae9e8d4
RaviTheCoder <rrravitesh@gmail.com>
2022-10-09 22:04:39 +1300

    Complete Ex 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,10 +1,16 @@
-const ftoc = function() {
-
+const ftoc = function(farenheit) {
+  return Math.round(((farenheit - 32) * .5556) * 10) / 10;
 };
 
-const ctof = function() {
+ftoc(32);
 
+const ctof = function(celsius) {
+  // return Math.round(((celsius * 1.8) + 32) * 10) / 10;
+  let conversion = (celsius * 1.8) + 32;
+  return Math.round(conversion * 10) / 10;
 };
+
+ctof(73.2);
 
 // Do not edit below this line
 module.exports = {
