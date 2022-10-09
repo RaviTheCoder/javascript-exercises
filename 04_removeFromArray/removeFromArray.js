a082548156ca89bb80b77f105c5529d7e7830780
Ravitesh Ratnam <rrravitesh@gmail.com>
2022-10-09 17:25:17 +1300

    Complete Ex 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,20 +1,12 @@
 const removeFromArray = function(array, ...value) {
-    console.log(value.length);
-    console.log(value[0]);
+
     for (let i = 0; i < value.length; i++) {
         for (let j = 0; j < array.length; j++) {
-            
-            if (array[j] == value[i]) {
-                console.log(array[j]);
-                console.log(value[i]);
-                // console.log(array[i]);
-                // console.log(value);
+            if (array[j] === value[i]) {
                 array.splice(j, 1);
-                console.log(array);
             } 
         }
     }
-    console.log(array);
     return array;
 };
 
