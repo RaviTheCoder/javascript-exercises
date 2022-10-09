c813363abf4bd695ffc910ca1757842f73a54552
RaviTheCoder <rrravitesh@gmail.com>
2022-10-09 21:43:16 +1300

    Unimportant syntax fix

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -2,7 +2,7 @@ const leapYears = function(year) {
   
   if (year % 100 == 0 && year % 400 != 0) {
     return false;
-  } if (year % 4 == 0) {
+  } else if (year % 4 == 0) {
     return true;
   } else {
     return false;
