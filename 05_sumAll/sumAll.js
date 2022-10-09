071b8d7acaaa0613c81cd933c830047bd03a8a16
Ravi Ratnam <raviratnam@outlook.com>
2022-10-10 00:12:57 +1300

    Complete Ex 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    if (typeof num1 != "number" || typeof num2 != "number") {
+        return "ERROR";
+    }
 
+    let lowNum = Math.min(num1, num2);
+    let highNum = Math.max(num1, num2);
+    let finalScore = 0;
+
+    if (lowNum < 0) {
+        return "ERROR";
+    }
+
+    while (lowNum <= highNum) {
+        finalScore += lowNum;
+        lowNum++;
+    }
+    return finalScore;
 };
 
 // Do not edit below this line
