fa8e944f6a0f5aa8b4356e5c130f4e2d288f5831
Ravitesh Ratnam <rrravitesh@gmail.com>
2022-10-09 17:45:07 +1300

    Complete first Revision branch

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,9 +1,15 @@
-const removeFromArray = function() {
-    console.log('test');
-
+const removeFromArray = function(array, ...values) {
+    for (let v = 0; v < values.length; v++) {
+        for (let a = 0; a < array.length; a++) {
+            if (array[a] === values[v]) {
+                array.splice(a, 1);
+            }
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
 module.exports = removeFromArray;
 
-removeFromArray();
+removeFromArray([1, 2, 3, 4], 3);
