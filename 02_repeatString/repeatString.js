d50565ff574b73fa80de1ccafc075317c950b5f4
Ravitesh Ratnam <raviratnam@outlook.com>
2022-10-28 15:10:51 +1300

    Complete Ex 1 + 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,17 @@
-const repeatString = function() {
+const repeatString = function(text, num) {
+    let i = 0;
+    let final = '';
 
+    if (num < 0) {
+        return 'ERROR';
+    }
+
+    while (i < num) {
+        final += text;
+        i++;
+    }
+    
+    return final;
 };
 
 // Do not edit below this line
